8a553ca5884cf69a75a3d7cf99ca2d6f8f9ee295
zubair ahmed <zubairhasrat48@gmail.com>
2022-08-17 20:16:48 +0500

    add metadata url

--- smart-contract/constants/index.js
+++ smart-contract/constants/index.js
@@ -3,6 +3,6 @@ require("dotenv").config({ path: ".env" });
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = process.env.WHITELIST_CONTRACT_ADDRESS;
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-app-xi.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
